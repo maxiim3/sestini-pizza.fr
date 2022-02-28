3f77ea01ddd9ef31b88af00037526481cdbb0680
benoit3780 <ben3780@hotmail.fr>
2022-02-28 11:43:08 +0100

    mise à jour

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PizzaList from './components/PizzaList';
+
+const App = () => {
+   return (
+      <div>
+         <h1>Nos pizza à la carte</h1>
+         <PizzaList />
+      </div>
+   );
+};
+
+export default App;
