30ee08cb9a233e0159650fccba0f60fed72ae332
Maxime Tamburrini <maximemickael.contact@gmail.com>
2022-04-04 18:42:50 +0200

    ready to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,15 @@
-import React, {useEffect, useState} from 'react';
 import {Redirect, Route, Switch} from "react-router-dom";
-import Home from './components/homeApp/home';
-import Navigation from "./components/navigation";
-import {NousTrouver} from "./components/findUsApp/nousTrouver";
-import Page404 from "./components/page404";
-import {MainLogoMinify} from "./components/commons/icons";
-import {Pizzas} from "./components/laCarte/pizzas";
+import React from 'react'
 
 const App = () => {
 
-    const [navIsHidden, setNavIsHidden] = useState(true)
-    useEffect(() => {
-        if (navIsHidden) {
-            navMobile.current.hidden = true
-            iconNav.current.hidden = false
-        } else {
-            navMobile.current.hidden = false
-            iconNav.current.hidden = true
-        }
-    })
-    const iconNav = React.createRef()
-    const navMobile = React.createRef()
-
-    const handleShowNav = () => {
-        navIsHidden ? setNavIsHidden(false) : setNavIsHidden(true)
-        console.log(!navIsHidden)
-    }
-
     return (
         <React.StrictMode>
-            <Navigation navRef={navMobile} onClose={handleShowNav}/>
-            <MainLogoMinify refLogo={iconNav} onClick={handleShowNav} beige={true}/>
             <Switch>
-                <Route path='/nous-trouver' component={NousTrouver}/>
-                {/*region LaCarteRedirect*/}
-                {/*<Redirect from='/pizza:s?' to='/la-carte'/>*/}
-                {/*<Redirect from='/carte' to='/la-carte'/>*/}
-                {/*endregion*/}
-                <Route path='/la-carte' component={Pizzas}/>
-                <Route path='/page404' component={Page404}/>
-                {/*region homeRedirect*/}
-                {/*<Redirect from='/home' to='/bienvenus'/>*/}
-                {/*<Redirect from='/accueil' to='/bienvenus'/>*/}
-                <Redirect from='/' exact to='/bienvenus'/>
-                {/*endregion*/}
-                {/*<Route path='/bienvenu:s?' component={Home}/>*/}
-                <Route path='/bienvenus' render={() => <Home iconNavRef={iconNav} onOpenNav={handleShowNav}/>}/>
-                <Redirect to='/page404'/>
+                <Route path='/bienvenus'>
+                    <h1>👋 Hello</h1>
+                </Route>
+                <Redirect from ='/*' to={'/bienvenus'}/>
             </Switch>
         </React.StrictMode>
     )
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.css'
-import 'font-awesome/css/font-awesome.css'
 import App from './App';
 import "./styles/index.css";
 
